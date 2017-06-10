0b716b14869a4eb9d3a58020be4c65323b6d99d0
John Shepler <john.sheplerjr@gmail.com>
2017-06-10 17:36:36 -0600

    routing

--- src/app/houses/house-detail/house-detail.component.ts
+++ src/app/houses/house-detail/house-detail.component.ts
@@ -1,21 +1,29 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { House } from "../house.model";
+import { ActivatedRoute} from '@angular/router'
 @Component({
   selector: 'app-house-detail',
   templateUrl: './house-detail.component.html',
   styleUrls: ['./house-detail.component.css']
 })
 export class HouseDetailComponent implements OnInit {
+    id:{id: number}
     house: House;
-
     lat: number = 30.6954;
     lng: number = -88.0399;
 
-  constructor() {
+
+  constructor(private route: ActivatedRoute) {
     this.house = new House(1, '9411 Smokewood Dr', 'Mobile', 'AL', '36695', 3, 1, "My House", 'https://s3-us-west-1.amazonaws.com/realestate222/house1.jpg', 1800)
   }
 
   ngOnInit() {
+      this.id = {
+          id: this.route.snapshot.params['id']
+      }
+      console.log(this.id)
+
   }
+
 
 }
